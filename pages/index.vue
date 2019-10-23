10c48b5853c6c68633c102d973f69db4ef096d08
Darren Booker <darrenbooker@Darrens-MacBook-Pro.local>
2019-10-22 20:03:55 -0700

    update vuetify grid system

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,54 @@
 <template>
-  <v-layout
+  <v-card>
+    <v-container class="fluid grid-list-md">
+      <v-layout class="row wrap">
+        <v-flex v-for="product in products" xs6 :key="product.title">
+          <v-card>
+            <v-img :src="product.pic" class="media" height="300px">
+              <v-container class="fill-height fluid">
+                <v-layout>
+                  <v-flex class="xs12 align-enter flexbox">
+                    <span class="display-1 yellow-text"  v-text="product.title"></span>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-img>
+            <v-card-actions>
+              <v-btn icon>
+                <v-icon>
+                  mdi-heart
+                </v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>
+                  mdi-share
+                </v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>
+                 mdi-cart-plus
+                </v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-card>
+</template>
+
+<!-- <v-layout
     column
     justify-center
     align-center
   >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
         <h1>Hello World</h1>
         <li v-for="(product, key) in products" :key="key">
           <ul>
             {{product.title}}
           </ul>
         </li>
-      </div>
-    </v-flex>
-  </v-layout>
-</template>
+  </v-layout> -->
 
 <script>
 import { db } from '../plugins/firebase'
@@ -39,3 +68,10 @@ export default {
   }
 };
 </script>
+<style >
+
+
+  .media:hover {
+    opacity: 0.4;
+  }
+</style>
